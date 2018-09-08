8d618470c9b94407444c86cc243570fb757b530f
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-08 16:26:39 +0530

    Create Appending_answer_to_end.java

---
+++ Appending_answer_to_end.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Appending_Answer_to_End
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String firststr=sc.next();
+System.out.println(firststr+" "+"Answer");
+}
+}
