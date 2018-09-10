4b2e67a628445cbb8ac1181a528ff8ce7e2b714e
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-10 09:55:24 +0530

    Update Maximum_of_the_two.java

--- Maximum_of_the_two.java
+++ Maximum_of_the_two.java
@@ -1,7 +1,9 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class Maximum_of_the_2
 {
-public static void main(String args[])
+public static void main(String args[]) throws java.lang.Exception
 {
 int n,count=0;
 Scanner sc=new Scanner(System.in);
