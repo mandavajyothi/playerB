838d74da04078c2554b87e3dd33932b2bfcec051
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-10 11:37:47 +0530

    Create Bitwise_NOT_of_the_number.java

---
+++ Bitwise_NOT_of_the_number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Bitwise_NOT_of_the_number
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n,num;
+n=sc.nextInt();
+System.out.println(~n);
+}
+}
