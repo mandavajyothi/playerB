be8cfb216f671b06818467c92c02a18f2bd380de
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-10 09:47:36 +0530

    Create Maximum_of_the_two.java

---
+++ Maximum_of_the_two.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Maximum_of_the_2
+{
+public static void main(String args[])
+{
+int n,count=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+	if(a[i]>a[i+1])
+	{
+		System.out.print(a[i]+" ");
+		
+	}
+	else
+	System.out.print(a[i+1]+" ");
+	
+}
+}
+}
