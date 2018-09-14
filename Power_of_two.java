3aa14e6046ee0476a9b7fc769c36419b29e18ce0
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-14 12:38:27 +0530

    Create Power_of_two.java

---
+++ Power_of_two.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class Power_of_two
+{
+static boolean isPowerOfTwo(int n)
+{
+if(n==0)
+return false;
+while(n!=1)
+{
+if(n%2!=0)
+return false;
+n=n/2;
+}
+return true;
+}
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int number=sc.nextInt();
+if (isPowerOfTwo(number))
+System.out.println("yes");
+else
+System.out.println("no");
+}
+}
