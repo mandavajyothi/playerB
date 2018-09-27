3968edfa833e5fe463fcb31951392e856b26de3c
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-27 14:55:00 +0530

    Create String_length_is_prime_or_not.java

---
+++ String_length_is_prime_or_not.java
@@ -0,0 +1,40 @@
+import java.io.*; 
+import java.util.*;
+class String_prime { 
+      
+  
+    static boolean isPrimeString(String str) 
+    { 
+        int len = str.length(), n = 0; 
+        for (int i = 0; i < len; i++) 
+            n += (int)str.charAt(i); 
+      
+        
+        if (n <= 1) 
+            return false; 
+        if (n <= 3) 
+            return true; 
+      
+        
+        if (n % 2 == 0 || n % 3 == 0) 
+            return false; 
+      
+        for (int i = 5; i * i <= n; i = i + 6) 
+            if (n % i == 0 || n % (i + 2) == 0) 
+                return false; 
+      
+        return true; 
+    } 
+  
+    
+    public static void main (String[] args) 
+    { 
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        if(isPrimeString(str)) 
+            System.out.println("yes"); 
+        else
+            System.out.println("no"); 
+    } 
+} 
