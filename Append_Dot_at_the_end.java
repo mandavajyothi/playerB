38fdb9fdd448192d2ff580d537c4745c08e1c2bd
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-09-27 16:17:15 +0530

    Create Append_Dot_at_the_end.java

---
+++ Append_Dot_at_the_end.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Append_Dot_At_end
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.nextLine();
+		System.out.println(str1+".");
+		
+	}
+}
