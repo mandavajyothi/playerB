9c3fed8577093ad3a0804f8ed808bfa3622ea1ff
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2018-10-06 15:41:53 +0530

    Create Equation.java

---
+++ Equation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Equation
+{
+	public static void main(String args[])
+	{
+		int a,b,y,x;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		x=sc.nextInt();
+		y=((a*x)+b);
+		System.out.println(y);
+	}
+}
