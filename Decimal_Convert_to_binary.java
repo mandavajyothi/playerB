60c56ae4c0094058e40be00551d0cdee865326b7
mandavajyothi <41770552+mandavajyothi@users.noreply.github.com>
2019-01-19 10:26:35 +0530

    Update Decimal_Convert_to_binary.java

--- Decimal_Convert_to_binary.java
+++ Decimal_Convert_to_binary.java
@@ -2,29 +2,20 @@ import java.io.*;
  import java.util.*;
 class Decimal_convert_to_binary 
 {
-    
-    static void decToBinary(int n)
+   static void decToBinary(int n)
     {
-        
         int[] binaryNum = new int[1000];
-  
-      
         int i = 0;
         while (n > 0) 
         {
-            
             binaryNum[i] = n % 2;
             n = n / 2;
             i++;
         }
-  
-      
-        for (int j = i - 1; j >= 0; j--)
+      for (int j = i - 1; j >= 0; j--)
             System.out.print(binaryNum[j]);
     }
-     
-   
-    public static void main (String[] args) 
+   public static void main (String[] args) 
     {
         int n;
         Scanner sc=new Scanner(System.in);
